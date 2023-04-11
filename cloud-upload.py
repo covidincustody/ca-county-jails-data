de0b2857dbb3de413c293900e432ca2cf50ea805
Covid In-Custody Project <117561631+covidincustody@users.noreply.github.com>
2023-04-11 12:56:39 -0700

    Update cloud-upload.py

--- cloud-upload.py
+++ cloud-upload.py
@@ -1,5 +1,33 @@
 # Only for team members with permission to edit live dataset
 
+import pandas as pd
+import os
+import gspread
+import oauth2client
+import oauth2client.service_account
+from utils import *
+
+
+# Generate variable lists
+select_vars = ['Active Cases',
+             'Confirmed Cases',
+             'Tests',
+             'Deaths',
+             'Pending Tests',
+             'Population', 
+             'Fully Vaccinated',
+             'Boosted', 
+             'Percent of Population Fully Vaccinated',
+             'Percent of Population Boosted']
+
+# Aggregating county jail data across all counties using the specified variables 
+all_county_jails_data = gen_county_jails_dataset(read_path = 'C:/Users/apkom/Downloads/Data Collection - County Jails',
+                                               select_vars = select_vars,
+                                               write_path = 'C:/Users/apkom/Documents/COVID19_Data_Analysis_CA_Incarcerated_Jails/All_Data/All_Data.xlsx') 
+
+recent_var_data = gen_recent_vals(county_jail_data = all_county_jails_data, 
+                                  write_path = "C:/Users/apkom/Documents/COVID19_Data_Analysis_CA_Incarcerated_Jails/All_Data/Recent_Data.xlsx")
+
 
 # Uploading aggregated county jails data to spreadsheet online (in Google Cloud)
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
